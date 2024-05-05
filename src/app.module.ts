3086f607c2eb15ebd83b604d238a34f7381cf935
rgazquez <rgazquez@seyte.com>
2024-05-05 20:29:47 +0200

    create auth & health modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './contexts/auth/auth.module';
+import { HealthModule } from './contexts/health/health.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, HealthModule],
   controllers: [AppController],
   providers: [AppService],
 })
