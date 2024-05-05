dc26d2d81fe389db73751b8e440c87d3224669a5
rgazquez <rgazquez@seyte.com>
2024-05-05 20:31:05 +0200

    remove unused code from app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './contexts/auth/auth.module';
 import { HealthModule } from './contexts/health/health.module';
 
 @Module({
   imports: [AuthModule, HealthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
