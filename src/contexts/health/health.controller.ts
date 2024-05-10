7a37ce8511cb5aa0138d26601e4b7de88f61bce8
rgazquez <rgazquez@seyte.com>
2024-05-11 00:29:51 +0200

    add prisma and health module

---
+++ src/contexts/health/health.controller.ts
@@ -0,0 +1,40 @@
+import { Controller, Get } from '@nestjs/common';
+import {
+  DiskHealthIndicator,
+  HealthCheck,
+  HealthCheckService,
+  MemoryHealthIndicator,
+  PrismaHealthIndicator,
+} from '@nestjs/terminus';
+import { PrismaClient } from '@prisma/client';
+
+@Controller()
+export class HealthController {
+  constructor(
+    private readonly health: HealthCheckService,
+    private readonly db: PrismaHealthIndicator,
+    private readonly disk: DiskHealthIndicator,
+    private readonly memory: MemoryHealthIndicator,
+  ) {}
+
+  @Get()
+  checkApp() {
+    return 'I am alive!';
+  }
+
+  @Get('health')
+  @HealthCheck()
+  check() {
+    const prismaClient = new PrismaClient();
+    return this.health.check([
+      () => this.db.pingCheck('database', prismaClient),
+      () =>
+        this.disk.checkStorage('storage', {
+          path: '/usr/src/app',
+          thresholdPercent: 10 * 1024 * 1024 * 1024,
+        }),
+      () => this.memory.checkHeap('memory_heap', 150 * 1024 * 1024),
+      () => this.memory.checkRSS('memory_rss', 150 * 1024 * 1024),
+    ]);
+  }
+}
